93e0ea961570cdf4e357d497d8f24b99193ed5c6
mika <strarti@bk.ru>
2025-03-22 13:36:39 +0600

    added firebase

--- src/components/Tasks/TaskItem/TaskItem.jsx
+++ src/components/Tasks/TaskItem/TaskItem.jsx
@@ -6,7 +6,7 @@ const TaskItem = ({value,isFinished, handleChange, setFinished, removeTask}) =>{
             <button onClick={setFinished} className={`${style.list__complete_btn} ${
                 isFinished ? style.completed : style.pending
                 }`}>✓️</button>
-            <input type='text' rows={1} className={style.list__text_area} value={value} onChange={handleChange}></input>
+            <input type='text' rows={1} className={style.list__text_area} value={value} onChange={handleChange} disabled={isFinished}></input>
             <button onClick={removeTask} className={style.list__remove_btn}>X</button>
         </div>
     )
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,12 +1,85 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import style from './tasks.module.css'
 import TaskItem from './TaskItem/TaskItem'
 
+import { getFirestore, collection, getDocs, addDoc,doc, setDoc, deleteDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCh6GBHtW4w7h_YPpSHiLUiQgQd8bTsDns",
+    authDomain: "tasks-443a6.firebaseapp.com",
+    projectId: "tasks-443a6",
+    storageBucket: "tasks-443a6.firebasestorage.app",
+    messagingSenderId: "668443635193",
+    appId: "1:668443635193:web:a1dcb81557623e78306128",
+    measurementId: "G-EKTMHP7S2C"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+  
+
 const Tasks = () => {
     const [tasks, setTask] = useState([])
+
+    useEffect(() => {
+      const fetchTasks = async () => {
+        const querySnapshot = await getDocs(collection(db, "tasks"));
+        const tasksData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setTask(tasksData);
+      };
+    
+      fetchTasks();
+    }, []);
+
+    async function addData(newData) {
+        try {
+          // Указываем коллекцию, в которую хотим добавить данные
+          const docRef = await addDoc(collection(db, "tasks"), newData);
+          newData.id = docRef.id
+          setTask([...tasks, newData])
+          console.log("Документ добавлен с ID: ", docRef.id);
+        } catch (error) {
+          console.error("Ошибка при добавлении документа: ", error);
+        }
+    }
+
+    async function updateTask(taskId, newData, index) {
+        try {
+            // Указываем путь к документу, который нужно обновить
+            const taskRef = doc(db, "tasks", taskId);
+        
+            // Обновляем данные с опцией merge
+            await setDoc(taskRef, newData[index], { task: newData[index].task, isFinished: newData[index].isFinished });
+            
+            console.log("Документ успешно обновлен!");
+          } catch (error) {
+            console.error("Ошибка при обновлении документа: ", error);
+        }
+    }
+
+    async function deleteTask(taskId) {
+        try {
+          // Указываем путь к документу, который нужно удалить
+          const taskRef = doc(db, "tasks", taskId);
+      
+          // Удаляем документ
+          await deleteDoc(taskRef);
+      
+          console.log("Документ успешно удален!");
+        } catch (error) {
+          console.error("Ошибка при удалении документа: ", error);
+        }
+      }
+    
     const addTask = () =>{
-        const newTasks = {task:'', isFinished:false}
-        setTask([...tasks, newTasks])
+        const newTasks = {id:'', task:'', isFinished:false}
+        addData(newTasks)
+        
     }
     const onTaskChange = (event, index) => {
         const newTasks = tasks.map((task, i) => {
@@ -15,6 +88,7 @@ const Tasks = () => {
           }
           return task;
         });
+        updateTask(newTasks[index].id, newTasks, index)
         setTask(newTasks);
     };
     const setFinished = (index) =>{
@@ -26,7 +100,12 @@ const Tasks = () => {
             }
             return task;
         });
+        updateTask(newTasks[index].id, newTasks, index)
         setTask(newTasks);
+    }
+    const removeTask = (index) =>{
+        deleteTask(tasks[index].id)
+        setTask(tasks.filter((_, i) => i !== index))
     }
     return(
         <div className={style.list}>
@@ -37,7 +116,7 @@ const Tasks = () => {
                 isFinished={task.isFinished}
                 handleChange={(event) => onTaskChange(event, index)}
                 setFinished={() => setFinished(index)}
-                removeTask={() => setTask(tasks.filter((_, i) => i !== index))}
+                removeTask={() => removeTask(index)}
                 />
             ))}
             <button onClick={addTask} className={style.list__add_btn}>+Add task</button>
